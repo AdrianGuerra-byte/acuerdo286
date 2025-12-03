d0a1e29c83725ad6781bf932fb60aaf9ffaefd1b
Adrian Guerra <adrian.guerra@cuh.mx>
2025-12-03 12:23:13 -0600

    Fix

--- components/registration-process-section.tsx
+++ components/registration-process-section.tsx
@@ -63,7 +63,7 @@ export function RegistrationProcessSection() {
               className="hidden lg:block absolute top-16 left-0 right-0 h-0.5 bg-border"
               style={{ left: "10%", right: "10%" }}
             />
-
+                        
             <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-6 lg:gap-4">
               {procesoData.map((step) => {
                 const Icon = iconMap[step.icon]
